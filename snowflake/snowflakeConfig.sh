4b028c95bd8386f43734411db9c0f2dce9f3f4b4
rajeshkannane <rajeshkannan.e@gmail.com>
2022-12-17 22:24:20 +0000

    Change from cloud system

--- snowflake/snowflakeConfig.sh
+++ snowflake/snowflakeConfig.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-
+set -e
+cd ~
 sudo curl -O https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.2/linux_x86_64/snowsql-1.2.24-linux_x86_64.bash
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 630D9F3CAB551AF3
 sudo curl -O https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.2/linux_x86_64/snowsql-1.2.24-linux_x86_64.bash.sig
 gpg --verify snowsql-1.2.24-linux_x86_64.bash.sig snowsql-1.2.24-linux_x86_64.bash
 SNOWSQL_DEST=~/bin SNOWSQL_LOGIN_SHELL=~/.profile bash snowsql-1.2.24-linux_x86_64.bash
-snowsql -a vc70422.europe-west2.gcp -u RAJESHKANNANE
+cp ~/.snowsql/config ~/.snowsql/configCopy
+cat ~/.snowsql/configCopy |sed --expression "s@../snowsql_rt.log@~/.snowsql_rt.log@; s@# log_bootstrap_file@log_bootstrap_file@" > ~/.snowsql/config
+echo "[connections.snowsqlTest]" >> ~/.snowsql/config
+echo "accountname = vc70422.europe-west2.gcp" >> ~/.snowsql/config
+echo "username = RAJESHKANNANE" >> ~/.snowsql/config
+echo "password = 070590+Raju" >> ~/.snowsql/config
+echo "dbname = testrun" >> ~/.snowsql/config
+echo "schemaname = TESTSCHEMA" >> ~/.snowsql/config
+echo "warehousename = compute_wh" >> ~/.snowsql/config
+#accountname = vc70422.europe-west2.gcp  
+#region = defaultregion
+#username = RAJESHKANNANE
+#password = 070590+Raju
+#dbname = testrun
+#schemaname = TESTSCHEMA
+#warehousename = compute_wh
+#snowsql -c snowsqlTest
